c11eaa2171ddfb983c08d0f9a976bccd3c802f02
Casey Marshall <casey.marshall@snyk.io>
2021-06-21 17:15:08 -0500

    Clear out references to force inlining of all $ref'd components.

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/getkin/kin-openapi v0.63.0
 	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 )
--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/url"
 	"os"
 	"path/filepath"
+	"reflect"
 
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/ghodss/yaml"
+	"github.com/mitchellh/reflectwalk"
 )
 
 func main() {
@@ -18,27 +19,46 @@ func main() {
 		log.Fatalf("failed to load schema from %q: %v", schemaFile, err)
 	}
 
+	// kin-openapi has a preference for displaying refs rather than inlining types,
+	// even though the references are followed and parsed.
+	//
+	// Clearing the Ref field causes the referenced component to be inlined
+	// when the OpenAPI document root is marshaled.
+	err = removeRefs(t)
+	if err != nil {
+		log.Fatalf("failed to remove refs: %v", err)
+	}
+
 	err = printSchemaYAML(t)
 	if err != nil {
 		log.Fatalf("failed to print schema: %v", err)
 	}
 }
 
-func printSchemaYAML(t *openapi3.T) error {
-	jsonBuf, err := t.MarshalJSON()
-	if err != nil {
-		return fmt.Errorf("failed to marshal JSON: %w", err)
+type refRemover struct{}
+
+func (*refRemover) Struct(reflect.Value) error {
+	return nil
+}
+func (*refRemover) StructField(sf reflect.StructField, v reflect.Value) error {
+	// Note this unconditionally clears the $ref URI, inlining all referenced
+	// components. We might be more selective about this by adding a bit of
+	// state to the walker.
+	// TODO: might also check for a `json:"$ref"` tag on the field.
+	if sf.Name == "Ref" && sf.Type.Kind() == reflect.String {
+		v.Set(reflect.ValueOf(""))
 	}
-	yamlBuf, err := yaml.JSONToYAML(jsonBuf)
-	if err != nil {
-		return fmt.Errorf("failed to convert JSON to YAML: %w", err)
-	}
-	_, err = fmt.Printf(string(yamlBuf))
-	return err
+	return nil
+}
+
+func removeRefs(t *openapi3.T) error {
+	return reflectwalk.Walk(t, &refRemover{})
 }
 
 func loadSchemaFile(schemaFile string) (*openapi3.T, error) {
 	var err error
+	// `cd` to the path containing the schema file, so that relative paths
+	// resolve.
 	schemaDir := filepath.Dir(schemaFile)
 	schemaDir, err = filepath.Abs(schemaDir)
 	if err != nil {
@@ -55,15 +75,18 @@ func loadSchemaFile(schemaFile string) (*openapi3.T, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to load %q: %w", schemaFile, err)
 	}
-
-	refUrlStr := "file://" + schemaDir
-	refUrl, err := url.Parse(refUrlStr)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse URL %q: %w", refUrlStr, err)
-	}
-	err = l.ResolveRefsIn(t, refUrl)
-	if err != nil {
-		return nil, fmt.Errorf("failed to resolve refs in OpenAPI spec: %w", err)
-	}
 	return t, nil
+}
+
+func printSchemaYAML(t *openapi3.T) error {
+	jsonBuf, err := t.MarshalJSON()
+	if err != nil {
+		return fmt.Errorf("failed to marshal JSON: %w", err)
+	}
+	yamlBuf, err := yaml.JSONToYAML(jsonBuf)
+	if err != nil {
+		return fmt.Errorf("failed to convert JSON to YAML: %w", err)
+	}
+	_, err = fmt.Printf(string(yamlBuf))
+	return err
 }
