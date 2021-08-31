4e1b8106c7ec0ff046e213466349e448483558d5
Casey Marshall <casey.marshall@snyk.io>
2021-08-31 13:53:50 -0500

    feat: Flags to control linting during compile, lint subcommand
    When running vervet on file change in `npm run dev`, we don't want to
    fail on linting errors. Instead, linting should be done as part of
    `npm run lint`.
    
    This breaks up the compile command to support these options.
    `vervet compile` can be run without linting (with linting being enabled
    by default). `vervet lint` can also be run to check the input and output
    files without recompiling them.

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -37,6 +37,11 @@ var App = &cli.App{
 				Aliases: []string{"c", "conf"},
 				Usage:   "Project configuration file",
 			},
+			&cli.BoolFlag{
+				Name:  "lint",
+				Usage: "Enable linting during build",
+				Value: true,
+			},
 			&cli.StringFlag{
 				Name:    "include",
 				Aliases: []string{"I"},
@@ -44,6 +49,18 @@ var App = &cli.App{
 			},
 		},
 		Action: Compile,
+	}, {
+		Name:      "lint",
+		Usage:     "Lint  versioned resources",
+		ArgsUsage: "[input resources root] [output api root]",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "config",
+				Aliases: []string{"c", "conf"},
+				Usage:   "Project configuration file",
+			},
+		},
+		Action: Lint,
 	}, {
 		Name:      "localize",
 		Usage:     "Localize references and validate a single OpenAPI spec file",
@@ -59,6 +76,23 @@ var App = &cli.App{
 
 // Compile compiles versioned resources into versioned API specs.
 func Compile(ctx *cli.Context) error {
+	project, err := projectFromContext(ctx)
+	if err != nil {
+		return err
+	}
+	return runCompiler(ctx, project, ctx.Bool("lint"), true)
+}
+
+// Lint checks versioned resources against linting rules.
+func Lint(ctx *cli.Context) error {
+	project, err := projectFromContext(ctx)
+	if err != nil {
+		return err
+	}
+	return runCompiler(ctx, project, true, false)
+}
+
+func projectFromContext(ctx *cli.Context) (*config.Project, error) {
 	var project *config.Project
 	if ctx.Args().Len() == 0 {
 		var configPath string
@@ -69,12 +103,12 @@ func Compile(ctx *cli.Context) error {
 		}
 		f, err := os.Open(configPath)
 		if err != nil {
-			return fmt.Errorf("failed to open %q: %w", configPath, err)
+			return nil, fmt.Errorf("failed to open %q: %w", configPath, err)
 		}
 		defer f.Close()
 		project, err = config.Load(f)
 		if err != nil {
-			return err
+			return nil, err
 		}
 	} else {
 		api := &config.API{
@@ -96,22 +130,31 @@ func Compile(ctx *cli.Context) error {
 			},
 		}
 	}
+	return project, nil
+}
 
+func runCompiler(ctx *cli.Context, project *config.Project, lint, build bool) error {
 	comp, err := compiler.New(ctx.Context, project)
 	if err != nil {
 		return err
 	}
-	err = comp.LintResourcesAll(ctx.Context)
-	if err != nil {
-		return err
+	if lint {
+		err = comp.LintResourcesAll(ctx.Context)
+		if err != nil {
+			return err
+		}
 	}
-	err = comp.BuildAll(ctx.Context)
-	if err != nil {
-		return err
+	if build {
+		err = comp.BuildAll(ctx.Context)
+		if err != nil {
+			return err
+		}
 	}
-	err = comp.LintOutputAll(ctx.Context)
-	if err != nil {
-		return err
+	if lint {
+		err = comp.LintOutputAll(ctx.Context)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
