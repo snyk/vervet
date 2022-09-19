92c44eb284bee7d7a4af24a9937131885cb1e019
Casey Marshall <casey.marshall@snyk.io>
2022-09-19 16:33:21 -0500

    feat: vervet filter command
    Introducing a new subcommand `vervet filter`, which filters OpenAPI
    documents. This is primarily useful from the CLI when wanting to
    generate code or documentation for a subset of the paths in a large API.
    Filtering the input OpenAPI document enables this kind of generated
    content to be more concise and focused.
    
    Astute readers may notice an overlap in functionality here with the
    RemoveElements function used by VU. A future refactoring should
    consolidate RemoveElements into a more generalized Filter that can be
    used by both VU and the CLI.

---
+++ internal/cmd/filter_test.go
@@ -0,0 +1,71 @@
+package cmd_test
+
+import (
+	"os"
+	"testing"
+
+	qt "github.com/frankban/quicktest"
+
+	"github.com/snyk/vervet/v4"
+	"github.com/snyk/vervet/v4/internal/cmd"
+	"github.com/snyk/vervet/v4/testdata"
+)
+
+func TestFilterInclude(t *testing.T) {
+	c := qt.New(t)
+	tmpOut := c.TempDir()
+
+	c.Run("filter include hello-world", func(c *qt.C) {
+		stdout, err := os.Create(tmpOut + "/spec.yaml")
+		c.Assert(err, qt.IsNil)
+		defer stdout.Close()
+		c.Patch(&os.Stdout, stdout)
+		err = cmd.Vervet.Run([]string{"vervet", "filter", "-I", "/examples/hello-world/{id}", testdata.Path("output/2021-06-01~experimental/spec.json")})
+		c.Assert(err, qt.IsNil)
+	})
+
+	out, err := os.ReadFile(tmpOut + "/spec.yaml")
+	c.Assert(err, qt.IsNil)
+	c.Log(string(out))
+
+	doc, err := vervet.NewDocumentFile(tmpOut + "/spec.yaml")
+	c.Assert(err, qt.IsNil)
+
+	// Included paths and their referenced components are present
+	c.Assert(doc.Paths["/examples/hello-world/{id}"], qt.Not(qt.IsNil))
+	c.Assert(doc.Components.Schemas["HelloWorld"], qt.Not(qt.IsNil))
+
+	// Not-included paths are not present
+	c.Assert(doc.Paths["/openapi"], qt.IsNil)
+}
+
+func XestFilterExclude(t *testing.T) {
+	c := qt.New(t)
+	tmpOut := c.TempDir()
+
+	c.Run("filter hello-world", func(c *qt.C) {
+		stdout, err := os.Create(tmpOut + "/spec.yaml")
+		c.Assert(err, qt.IsNil)
+		defer stdout.Close()
+		app := cmd.NewApp(&cmd.CLIApp, cmd.VervetParams{
+			Stdin:  os.Stdin,
+			Stdout: stdout,
+			Stderr: os.Stderr,
+			Prompt: cmd.Prompt{},
+		})
+		err = app.Run([]string{"vervet", "filter", "-X", "/examples/hello-world/{id}", testdata.Path("output/2021-06-01~experimental/spec.json")})
+		c.Assert(err, qt.IsNil)
+	})
+
+	doc, err := vervet.NewDocumentFile(tmpOut + "/spec.yaml")
+	c.Assert(err, qt.IsNil)
+
+	// Excluded paths and components only these reference, are not present
+	c.Assert(doc.Paths["/examples/hello-world/{id}"], qt.IsNil)
+	c.Assert(doc.Components.Schemas["HelloWorld"], qt.IsNil)
+
+	// Not-excluded paths and referenced components are present
+	c.Assert(doc.Paths["/openapi"], qt.Not(qt.IsNil))
+	c.Assert(doc.Paths["/openapi/{version}"], qt.Not(qt.IsNil))
+	c.Assert(doc.Components.Headers["VersionRequestedResponseHeader"], qt.Not(qt.IsNil))
+}
