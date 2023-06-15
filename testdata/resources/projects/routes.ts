b18c2c7a2a826740ed29d83c4a9ecd1a32f33452
Mike Smart <mike.smart@snyk.io>
2023-06-15 14:20:12 +0100

    chore: commit generated code from new resource versions

--- testdata/resources/projects/routes.ts
+++ testdata/resources/projects/routes.ts
@@ -1,11 +1,17 @@
 import { versions } from '@snyk/rest-node-libs';
 import * as v2021_08_20 './2021-08-20';
+import * as v2023_06_03 './2023-06-03';
 import * as v2021_06_04 './2021-06-04';
 
 export const deleteOrgsProject = versions([
   {
     handler: v2021_08_20.deleteOrgsProject,
     version: '2021-08-20~experimental',
+  },
+
+  {
+    handler: v2023_06_03.deleteOrgsProject,
+    version: '2023-06-03~experimental',
   },
 ]);
 export const getOrgsProjects = versions([
--- testdata/resources/users/routes.ts
+++ testdata/resources/users/routes.ts
@@ -1,9 +1,15 @@
 import { versions } from '@snyk/rest-node-libs';
 import * as v2023_06_01 './2023-06-01';
+import * as v2023_06_02 './2023-06-02';
 
 export const getUsers = versions([
   {
     handler: v2023_06_01.getUsers,
     version: '2023-06-01~experimental',
+  },
+
+  {
+    handler: v2023_06_02.getUsers,
+    version: '2023-06-02~experimental',
   },
 ]);
