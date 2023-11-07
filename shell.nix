fe75e7df60717c6bcd276346476376550e5ee07f
John Gresty <john.gresty@snyk.io>
2023-11-07 09:22:58 +0000

    fix: update nix flake after dependency upgrade
    Main change is updating the dependencies hash so it is up to date with
    the most recent changes.
    
    Additionally the devshell has been updated to use the latest node LTS
    and match the go version defined in go.mod, which required an update to
    nixpkgs.

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ let
     buildNodePackage = nodeEnv.buildNodePackage;
   };
 in pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; [ go_1_19 envsubst spectral ];
+  nativeBuildInputs = with pkgs.buildPackages; [ go_1_21 envsubst spectral ];
   shellHook = ''
     export GOPATH="$HOME/.cache/gopaths/$(sha256sum <<<$(pwd) | awk '{print $1}')"
   '';
