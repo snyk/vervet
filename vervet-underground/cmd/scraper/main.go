54e8ba1c2a950731340014cab0b530eea355b3aa
John Gresty <john.gresty@snyk.io>
2024-04-24 21:33:17 +0100

    feat: split scraper and api in vervet underground
    Previously we ran the scraper and api in the same process, with the
    scraper running before the api started listening. This caused us to
    experience a very long startup time before the api became available,
    which is not a fun experience when trying to push out an update. In
    addition if a scrape failed for any reason, such as an external service
    not being available, then it would take down this api - which knocked
    our api docs reference site offline.
    
    Furthermore this will allow us to scale our api without having to
    allocate the large amounts of resources that the scraping process needs.
    
    Note that this now does not run the scraping unless explicitly started
    by running the separate scraper binary. They are packaged together for
    convenience.

--- vervet-underground/cmd/scraper/main.go
+++ vervet-underground/cmd/scraper/main.go
@@ -6,15 +6,12 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"os/signal"
 	"time"
 
-	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
 	"vervet-underground/config"
-	"vervet-underground/internal/handler"
 	"vervet-underground/internal/scraper"
 	"vervet-underground/internal/storage"
 	"vervet-underground/internal/storage/disk"
@@ -24,13 +21,8 @@ import (
 
 func main() {
 	var wait time.Duration
-	var scrapeInterval time.Duration
 	var configJson string
 	var overlayFile string
-	flag.DurationVar(&wait, "graceful-timeout", time.Second*15,
-		"the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
-	flag.DurationVar(&scrapeInterval, "scrape-interval", time.Minute,
-		"the frequency at which scraping occurs  - e.g. 15s, 1m, 1h")
 	flag.StringVar(&configJson, "config-file", "config.default.json",
 		"the configuration file holding target services and the host address to run server on")
 	flag.StringVar(&overlayFile, "overlay-file", "",
@@ -43,8 +35,7 @@ func main() {
 	var cfg *config.ServerConfig
 	var err error
 	if cfg, err = config.Load(configJson); err != nil {
-		logError(err)
-		log.Fatal().Msg("unable to load config")
+		log.Fatal().Err(err).Msg("unable to load config")
 	}
 	log.Info().Msgf("services: %s", cfg.Services)
 
@@ -52,18 +43,14 @@ func main() {
 	if overlayFile != "" {
 		overlayContents, err = os.ReadFile(overlayFile)
 		if err != nil {
-			logError(err)
-			log.Fatal().Msgf("unable to load overlay file %q", overlayFile)
+			log.Fatal().Err(err).Msgf("unable to load overlay file %q", overlayFile)
 		}
 	}
 
-	// initialize Scraper
-	ticker := time.NewTicker(scrapeInterval)
 	ctx := context.Background()
 	st, err := initializeStorage(ctx, cfg, overlayContents)
 	if err != nil {
-		logError(err)
-		log.Fatal().Msg("unable to initialize storage client")
+		log.Fatal().Err(err).Msg("unable to initialize storage client")
 	}
 
 	sc, err := scraper.New(cfg, st, scraper.HTTPClient(&http.Client{
@@ -71,77 +58,13 @@ func main() {
 		Transport: scraper.DurationTransport(http.DefaultTransport),
 	}))
 	if err != nil {
-		logError(err)
-		log.Fatal().Msg("unable to load storage")
+		log.Fatal().Err(err).Msg("unable to load storage")
 	}
-	// initialize
+
 	err = runScrape(ctx, sc)
 	if err != nil {
-		log.Fatal().Err(err).Msg("failed initialization scraping of service")
+		log.Fatal().Err(err).Msg("failed scraping of service")
 	}
-
-	h := handler.New(cfg, st, handler.UseDefaultMiddleware)
-
-	srv := &http.Server{
-		Addr: fmt.Sprintf("%s:8080", cfg.Host),
-		// Good practice to set timeouts to avoid Slowloris attacks.
-		WriteTimeout: time.Second * 15,
-		ReadTimeout:  time.Second * 15,
-		IdleTimeout:  time.Second * 60,
-		Handler:      h,
-	}
-
-	quit := make(chan struct{}, 1)
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				if err := runScrape(ctx, sc); err != nil {
-					logError(err)
-				}
-			case <-quit:
-				ticker.Stop()
-				return
-			}
-		}
-	}()
-
-	// Run our server in a goroutine so that it doesn't block.
-	go func() {
-		log.Info().Msg(fmt.Sprintf("I'm starting my server on %s:8080", cfg.Host))
-		if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
-			logError(err)
-			os.Exit(1)
-		}
-	}()
-
-	c := make(chan os.Signal, 1)
-	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
-	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
-	signal.Notify(c, os.Interrupt)
-	// Block until we receive our signal.
-	<-c
-
-	// closes the scraper go routine
-	quit <- struct{}{}
-	close(quit)
-	log.Info().Msg("scraper successfully spun down")
-
-	// Create a deadline to wait for.
-	ctx, cancel := context.WithTimeout(context.Background(), wait)
-	defer cancel()
-	// Doesn't block if no connections, but will otherwise wait
-	// until the timeout deadline.
-	err = srv.Shutdown(ctx)
-	if err != nil {
-		logError(err)
-	}
-
-	// Optionally, you could run srv.Shutdown in a goroutine and block on
-	// <-ctx.Done() if your application should wait for other services
-	// to finalize based on context cancellation.
-	log.Info().Msg("shutting down cleanly")
-	os.Exit(0)
 }
 
 // runScrape runs scraping all services and can take
@@ -155,15 +78,6 @@ func runScrape(ctx context.Context, sc *scraper.Scraper) error {
 	}
 	log.Info().Msgf("scraper successfully completed run at %s", time.Now().UTC().String())
 	return nil
-}
-
-func logError(err error) {
-	log.
-		Error().
-		Stack().
-		Err(err).
-		Str("cause", fmt.Sprintf("%+v", errors.Cause(err))).
-		Msg("UnhandledException")
 }
 
 func initializeStorage(ctx context.Context, cfg *config.ServerConfig, overlayContents []byte) (storage.Storage, error) {
