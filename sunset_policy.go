d52d52a453ec70c832c7d8e41284fb9f34a2c9f6
Radu Tarean <radu.tarean@snyk.io>
2024-09-04 13:08:02 +0300

    feat: add sunset policy

---
+++ sunset_policy.go
@@ -0,0 +1,21 @@
+package vervet
+
+import (
+	"fmt"
+	"time"
+)
+
+// https://github.com/snyk/sweater-comb/blob/main/docs/version.md#stability-levels
+var VersionSunsetPolicy = map[Stability]time.Duration{
+	StabilityGA:           180 * 24 * time.Hour,
+	StabilityBeta:         90 * 24 * time.Hour,
+	StabilityExperimental: 30 * 24 * time.Hour,
+}
+
+func GetSunsetDate(v Version) (time.Time, error) {
+	duration, ok := VersionSunsetPolicy[v.Stability]
+	if !ok {
+		return time.Time{}, fmt.Errorf("unknown stability: %s", v.Stability)
+	}
+	return v.Date.Add(duration), nil
+}
