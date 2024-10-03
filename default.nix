95f73063e65400529d7b9455158642f50d776afe
John Gresty <john.gresty@snyk.io>
2024-10-03 09:51:45 +0100

    fix: inject version into nix flake
    The version is generated at build time, which was skipped in the nix
    package. This means that `vervet -version` would always report
    "develop".
    
    The version should be the same as the git tag, hopefully this will be
    identified by the current ref but will fall back to the last modified
    date.

--- default.nix
+++ default.nix
@@ -1,10 +1,17 @@
-{ buildGoModule, lib, lastMod }:
+{
+  buildGoModule,
+  lib,
+  version,
+}:
 buildGoModule rec {
+  inherit version;
+
   pname = "vervet";
-  version = builtins.substring 0 8 lastMod;
   src = ./.;
 
   vendorHash = "sha256-8UOyGj1//ydQuU9PHKNRG68xkKyI4Qz3eSKj8sqNqDc=";
+
+  preBuild = "VERSION=${version} bash ./scripts/genversion.bash";
 
   meta = with lib; {
     description = "API resource versioning tool";
--- flake.nix
+++ flake.nix
@@ -6,24 +6,49 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    let lastMod = self.lastModifiedDate or self.lastModified or "19700101";
-    in flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    let
+      lastMod = self.lastModifiedDate or self.lastModified or "";
+      version =
+        if (self ? shortRef) then
+          self.shortRef
+        else if (self ? ref) then
+          self.ref
+        else if (lastMod != "") then
+          builtins.substring 0 8 lastMod
+        else
+          "develop";
+    in
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
         ne = nixpkgs + "/pkgs/development/node-packages/node-env.nix";
         nodeEnv = import ne {
           inherit (pkgs)
-            stdenv lib python2 runCommand writeTextFile writeShellScript;
+            stdenv
+            lib
+            python2
+            runCommand
+            writeTextFile
+            writeShellScript
+            ;
           inherit pkgs;
           libtool = pkgs.darwin.cctools;
           nodejs = pkgs.nodejs;
         };
-      in rec {
+      in
+      rec {
         packages = flake-utils.lib.flattenTree {
-          default = pkgs.callPackage ./default.nix { inherit lastMod; };
+          default = pkgs.callPackage ./default.nix { inherit version; };
         };
         apps.default = flake-utils.lib.mkApp { drv = packages.default; };
         devShell = pkgs.callPackage ./shell.nix { inherit pkgs nodeEnv; };
-      });
+      }
+    );
 }
